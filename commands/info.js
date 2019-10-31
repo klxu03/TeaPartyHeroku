2e4358f1efb08e25e438763d1ec3891c26c23ad4
Kevin <beaver2025@gmail.com>
2019-10-31 17:31:05 -0400

    In !info edited with debugging and changed let to var
    Went ahead and added a bunch of console.logs to see what is going on with the variables. Went ahead and changed "let level" to "var level" so maybe it'll be more permanent and not only change inside the query?

--- commands/info.js
+++ commands/info.js
@@ -13,10 +13,10 @@ module.exports = class info {
     async run(bot, msg, args, con) {
         let target = msg.mentions.members.first() || msg.author;
 
-        let level; 
-        let exp; 
-        let multiplier; 
-        let condition = true;
+        var level; 
+        var exp; 
+        var multiplier = 20; 
+        var condition = true;
 
         con.query(`SELECT * FROM users WHERE discordid = '${target.id}'`, (err, rows) => {
             if (err) throw err;
@@ -32,23 +32,26 @@ module.exports = class info {
             level = rows[0].level;
             exp = rows[0].exp;
 
-            multiplier = 20;
             if (level < 20) multiplier = level; 
 
             msg.channel.send(exp + "/" + (multiplier * 20000) + " xp");
             msg.channel.send("Your level is " + level);
         })
 
-        let XPString = "" + exp + "/" + (multiplier * 20000) + " xp"
+        if (condition) {
+            let XPString = "" + exp + "/" + (multiplier * 20000) + " xp";
+            console.log("level is " + level);
+            console.log("exp is " + exp);
 
-        let profile = new Discord.RichEmbed()
-        .setAuthor(target)
-        .setColor("#00bcd4")
-        .addField("Current Level", level, true)
-        .addField("Current XP Inside Level", XPString, true)
-        .setFooter("Feel Free to DM Me if There Are Concerns");
+            let profile = new Discord.RichEmbed()
+            .setAuthor(target)
+            .setColor("#00bcd4")
+            .addField("Current Level", level, true)
+            .addField("Current XP Inside Level", XPString, true)
+            .setFooter("Feel Free to DM Me if There Are Concerns");
 
-        return msg.channel.send(profile);
+            return msg.channel.send(profile);
+        }
 
         //console.log("Hello " + msg.member.user.tag);
     }
